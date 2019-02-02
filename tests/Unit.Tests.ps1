bc44f5b75aa57f997c02870aa63570a0ab785952
luzkenin <warrenjr@cassetterunner.io>
2019-02-02 23:37:31 +0100

    trying tests

--- tests/Unit.Tests.ps1
+++ tests/Unit.Tests.ps1
@@ -9,6 +9,34 @@ if ((Split-Path $ModuleBase -Leaf) -eq 'Tests') {
 ## this variable is for the VSTS tasks and is to be used for refernecing any mock artifacts
 $Env:ModuleBase = $ModuleBase
 Import-Module $ModuleBase\$ModuleName.psd1 -PassThru -ErrorAction Stop | Out-Null
-Describe "Basic function unit tests" -Tags Build , Unit{
-
+Describe "Basic function unit tests" -Tags Unit{
+    Context "Validate Parameters" {
+        $paramCount = 3
+        $DefaultParamCount = 11
+        [object[]]$params = (Get-ChildItem function:\Backup-VSCode).Parameters.Keys
+        $KnownParameters = 'Path', 'Settings', 'Extensions'
+        It "Should contain our specific parameters" {
+            ( (Compare-Object -ReferenceObject $KnownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count) | Should Be $paramCount
+        }
+        It "Should only contain $ParamCount parameters" {
+            $params.Count - $DefaultParamCount | Should Be $paramCount
+        }
+    }
 }
+Describe "Basic function unit tests" -Tags Unit{
+    Context "Validate Parameters" {
+        $paramCount = 3
+        $DefaultParamCount = 11
+        [object[]]$params = (Get-ChildItem function:\Restore-VSCode).Parameters.Keys
+        $KnownParameters = 'Path', 'Settings', 'Extensions'
+        It "Should contain our specific parameters" {
+            ( (Compare-Object -ReferenceObject $KnownParameters -DifferenceObject $params -IncludeEqual | Where-Object SideIndicator -eq "==").Count) | Should Be $paramCount
+        }
+        It "Should only contain $ParamCount parameters" {
+            $params.Count - $DefaultParamCount | Should Be $paramCount
+        }
+    }
+}
+
+
+# clearly don't know what i'm doing here
