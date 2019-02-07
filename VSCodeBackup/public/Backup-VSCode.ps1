7a7161ba1e0652aec8e22bc693e5bb8c2f46a076
luzkenin <luzkenin@gmail.com>
2019-02-07 11:41:58 +0100

    fixed alias !deploy

--- VSCodeBackup/public/Backup-VSCode.ps1
+++ VSCodeBackup/public/Backup-VSCode.ps1
@@ -1,32 +1,33 @@
-function Backup-VSCode {
+function Backup-VSCode
+{
     <#
     .SYNOPSIS
     Backup VS Code settings and extensions
-    
+
     .DESCRIPTION
     Backup VS Code settings and extensions
-    
+
     .PARAMETER Path
     Location to store zip file
-    
+
     .PARAMETER Settings
     Switch to backup settings
-    
+
     .PARAMETER Extensions
     Switch to backup extensions
-    
+
     .EXAMPLE
     Backup-VSCode -Path c:\Users\bobby\Desktop -Settings -Extensions
-    
+
     .NOTES
     General notes
     #>
-    
+
     [CmdletBinding()]
     param (
         # Parameter help description
         [Parameter(Mandatory)]
-        [ValidateScript({Test-Path -Path $_})]
+        [ValidateScript( {Test-Path -Path $_})]
         [string]
         $Path,
         # Parameter help description
@@ -38,42 +39,52 @@ function Backup-VSCode {
         [switch]
         $Extensions
     )
-    
-    begin {
-        $TimeStamp = Get-Date -Format o | foreach {$_ -replace ":", "."}
+
+    begin
+    {
+        $TimeStamp = Get-Date -Format o | ForEach-Object {$_ -replace ":", "."}
         $Name = "VSCode-$($TimeStamp).zip"
         $Path = Resolve-Path -Path $Path
     }
-    
-    process {
+
+    process
+    {
         #Can't read some files while Code is running
         $CodeRunning = Get-Process -Name code -ErrorAction SilentlyContinue
-        
-        if($CodeRunning) {
+
+        if ($CodeRunning)
+        {
             Write-Verbose "Closing VS Code"
             $CodeRunning.CloseMainWindow() | Out-Null
         }
 
         $ExtensionsDirectory = "$env:USERPROFILE\.vscode" | Resolve-Path
         $SettingsDirectory = "$env:APPDATA\Code\User\settings.json" | Resolve-Path
-        if($Extensions.IsPresent) {
-            try {
+        if ($Extensions.IsPresent)
+        {
+            try
+            {
                 Compress-Archive -Path $ExtensionsDirectory -DestinationPath $Path\$Name -Update -CompressionLevel NoCompression
             }
-            catch {
+            catch
+            {
                 throw $_
             }
         }
-        if($Settings.IsPresent) {
-            try {
+        if ($Settings.IsPresent)
+        {
+            try
+            {
                 Compress-Archive -LiteralPath $SettingsDirectory -DestinationPath $Path\$Name -Update
             }
-            catch {
+            catch
+            {
                 throw $_
             }
         }
     }
-    
-    end {
+
+    end
+    {
     }
 }
