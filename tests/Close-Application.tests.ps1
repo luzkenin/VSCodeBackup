73dd1a913fa61ab4a4ff017c2742495e62f23757
luzkenin <luzkenin@gmail.com>
2019-02-20 14:41:18 +0100

    testing tests again omg

--- tests/Close-Application.tests.ps1
+++ tests/Close-Application.tests.ps1
@@ -1,4 +1,9 @@
-Import-Module VSCodeBackup.psd1 -Force
+if (Test-Path -Path "C:\MyProject") {
+    Import-Module C:\MyProject\VsCodeBackup\VSCodeBackup\VSCodeBackup.psd1 -Force
+}
+else {
+    Import-Module C:\git\VsCodeBackup\VSCodeBackup\VSCodeBackup.psd1 -Force
+}
 
 InModuleScope 'VSCodeBackup' {
     Describe "Close-Application" -Tag 'Build' {
