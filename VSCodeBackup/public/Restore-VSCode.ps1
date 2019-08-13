19cd946911835816d001fd1b2d7a0e2982f56922
luzkenin <luzkenin@gmail.com>
2019-08-13 14:25:54 +0200

    updated restore to work with core/5.1/linux/mac

--- VSCodeBackup/public/Restore-VSCode.ps1
+++ VSCodeBackup/public/Restore-VSCode.ps1
@@ -2,31 +2,31 @@ function Restore-VSCode {
     <#
     .SYNOPSIS
     Restore VS Code from a backup
-    
+
     .DESCRIPTION
     Restore VS Code from a backup
-    
+
     .PARAMETER Path
     Path to backup file
-    
+
     .PARAMETER Settings
     Switch to restore settings
-    
+
     .PARAMETER Extensions
     Switch to restore extensions
-    
+
     .EXAMPLE
     Restore-VSCode -Path .\VSCode-2019-01-31T23.33.58.3351871+01.00.zip -Settings -Extensions
-    
+
     .NOTES
     General notes
     #>
-    
+
     [CmdletBinding()]
     param (
-        # Parameter help description
+        # Path to zip file
         [Parameter(Mandatory)]
-        [ValidateScript({Test-Path -Path $_})]
+        [ValidateScript( { Test-Path -Path $_ })]
         [string]
         $Path,
         # Parameter help description
@@ -38,38 +38,44 @@ function Restore-VSCode {
         [switch]
         $Extensions
     )
-    
+
     begin {
         $Path = Resolve-Path -Path $Path
+        $TempPath = [system.io.path]::GetTempPath()
     }
-    
+
     process {
         #Can't write some files while Code is running
-        $CodeRunning = Get-Process -Name code -ErrorAction SilentlyContinue
-        
-        if($CodeRunning) {
+        $CodeRunning = Get-Process -Name "code" -ErrorAction SilentlyContinue
+
+        if ($CodeRunning) {
+            #Can't read some files while Code is running
             Write-Verbose "Closing VS Code"
-            $CodeRunning.CloseMainWindow() | Out-Null
+            try {
+                Close-Application -ApplicationName "code"
+            }
+            catch {
+                $_
+            }
         }
 
-        $ExtensionsDirectory = "$env:USERPROFILE\.vscode" | Resolve-Path
-        $SettingsDirectory = "$env:APPDATA\Code\User\settings.json" | Resolve-Path
+        $CodeDir = Get-CodeDirectory
 
         try {
-            Expand-Archive -Path $Path -DestinationPath $env:TEMP -force
+            Expand-Archive -Path $Path -DestinationPath $TempPath -force
         }
         catch {
             throw $_
         }
-        
-        if($Extensions.IsPresent) {
-            Copy-Item -Path "$env:TEMP\.vscode\extensions" -Destination $ExtensionsDirectory -Force -Recurse
+
+        if ($Extensions.IsPresent) {
+            Copy-Item -Path "$TempPath\.vscode\extensions" -Destination $CodeDir.ExtensionsDirectory -Force -Recurse
         }
-        if($Settings.IsPresent) {
-            Copy-Item -LiteralPath "$env:TEMP\settings.json" -Destination $SettingsDirectory -Force
+        if ($Settings.IsPresent) {
+            Copy-Item -LiteralPath "$TempPath\settings.json" -Destination $CodeDir.SettingsFile -Force
         }
     }
-    
+
     end {
     }
 }
